497d484b11d5d9bd72e04c969da7dd71e0492a4d
Deeshan Sharma <deeshansharma04@gmail.com>
2020-12-27 19:24:49 +0530

    feat: Added function to load json file

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ Author: Deeshan Sharma
 Date Created: December 27, 2020
 Purpose: This is a bot which will join the zoom meetings on your behalf automatically and mark your attendance.
 '''
+
+import json
+
+def getTime():
+    with open('timetable.json', 'r') as f:
+        timetable = json.load(f)
+    return timetable
