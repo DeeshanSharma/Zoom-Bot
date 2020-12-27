34f0171b49fcd983db9be72463bc4e0475fdd7ce
Deeshan Sharma <deeshansharma04@gmail.com>
2020-12-27 19:26:28 +0530

    feat: Added fuction to open zoom app

--- main.py
+++ main.py
@@ -5,8 +5,16 @@ Purpose: This is a bot which will join the zoom meetings on your behalf automati
 '''
 
 import json
+import subprocess
+import time
 
 def getTime():
     with open('timetable.json', 'r') as f:
         timetable = json.load(f)
     return timetable
+
+def openZoom():
+    subprocess.Popen(r"C:\Users\princ\AppData\Roaming\Zoom\bin\Zoom.exe")
+    time.sleep(20)
+
+timetable = getTime()
