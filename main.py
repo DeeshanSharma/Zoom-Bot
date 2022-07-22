47f5564fe556f0fc0372180bf20c643ea9789f58
matheesha-pathirana <pathiranamatheesha@gmail.com>
2022-07-22 11:16:18 +0530

    Automatically gets username

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-'''
+"""
 Author: Deeshan Sharma
 Date Created: December 27, 2020
-Purpose: This is a bot which will join the zoom meetings on your behalf automatically and mark your attendance.
-'''
+Purpose: This is a bot which will join the Zoom meetings on your behalf automatically and mark your attendance.
+"""
 
 import json
 from datetime import datetime as dt
@@ -10,11 +10,16 @@ import subprocess
 import time
 import pyautogui as gui
 import link
+import getpass
+
+username = getpass.getuser()
+
 
 def getTime():
     with open('timetable.json', 'r') as f:
         timetable = json.load(f)
     return timetable
+
 
 def checkClass(timetable):
     day = dt.today().strftime("%A").lower()
@@ -25,10 +30,12 @@ def checkClass(timetable):
             openZoom()
             time.sleep(240)
 
+
 def openZoom():
-    subprocess.Popen(r"C:\Users\princ\AppData\Roaming\Zoom\bin\Zoom.exe") # Change app location here
+    subprocess.Popen(fr"C:\Users\{username}\AppData\Roaming\Zoom\bin\Zoom.exe")  # Change app location here
     time.sleep(15)
     joinClass()
+
 
 def buttonClick(image):
     btn = gui.locateCenterOnScreen(image)
@@ -36,16 +43,19 @@ def buttonClick(image):
     gui.click()
     time.sleep(2)
 
+
 def buttonWrite(text):
     gui.write(text)
     time.sleep(1)
     gui.press('enter')
+
 
 def joinClass():
     buttonClick('images/main-join-btn.png')
     buttonWrite(link.ID)
     time.sleep(6)
     buttonWrite(link.PASS)
+
 
 timetable = getTime()
 checkClass(timetable)
